f1d35114347044df43f05d5ae2f48974dfba3765
hulkmania40 <ali.mubashir40@gmail.com>
2025-06-28 16:52:04 +0530

    Use env variables instead

--- src/utils/axiosClient.tsx
+++ src/utils/axiosClient.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000',  // Update if using a Vite proxy
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
